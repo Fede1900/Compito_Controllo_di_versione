2e3331b6dc5b6bd3c877696f829f1e57284cf7c5
DESKTOP-FEJUPGQ\feder <federome@live.it>
2023-03-09 16:18:18 +0100

    compilazione ciaoscript

--- Assets/script/CiaoScript.cs
+++ Assets/script/CiaoScript.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class CiaoScript : MonoBehaviour
 {
+    public int volte = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,10 @@ public class CiaoScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            volte++;
+            Debug.LogFormat("Ciao sono un cubo, mi hai premuto {0} volte", volte);
+        }
     }
 }
